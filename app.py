a90c8c86d554330525e81a32d6356a6735c42df3
Xapeck <xapeck@gmail.com>
2019-10-02 11:21:52 +0300

    Add files via upload

--- app.py
+++ app.py
@@ -1,27 +1,15 @@
-from flask import Flask
-import os
-import socket
+from flask import Flask 
+import os 
+import socket 
 
-app = Flask(__name__)
+app = Flask(__name__) 
 
-@app.route("/")
+@app.route("/") 
 def hello():
-    html = "<style>
-html{
-    width:100%;
-    height:100%;
-}
-body{
-    width:100%;
-    height:100%;
-    background-color:#DDD;
-}
-</style><h3>Inan Taksiajat (2.9.-11.10.2019)</h3>" \
-           "<b>MA: 8:15 - 13:40<br/>TI: 7:25 - 13:35<br/>KE:    8:15 - 12:20<br/>TO:    7:45 - 13:45<br/>PE:    7:25 - 13:25</b><br/>"
-\
-           "<b>Hostname:</b> {hostname}<br/>" \
-           "<b>Visits:</b> {visits}"
+    html = "<h3>Inan Taksiajat (2.9.-11.10.2019)</h3>" \
+	"<b>MA: 8:15 - 13:40<br/>TI: 7:25 - 13:35<br/>KE: 8:15 - 12:20<br/>TO: 7:45 - 13:45<br/>PE: 7:25 - 13:25</b><br/>" \
+           "<b>Hostname:</b> {hostname}<br/>" 
     return html.format(name=os.getenv("NAME", "world"), hostname=socket.gethostname())
 
 if __name__ == "__main__":
-    app.run(host='0.0.0.0', port=80)
+	app.run(host='0.0.0.0', port=80)
