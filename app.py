34395d3d6933ada1b35df7d99376d4614aa4c0a2
Xapeck <xapeck@gmail.com>
2019-10-02 10:16:58 +0300

    Update app.py

--- app.py
+++ app.py
@@ -6,7 +6,17 @@ app = Flask(__name__)
 
 @app.route("/")
 def hello():
-    html = "<h3>Inan Taksiajat (2.9.-11.10.2019)</h3>" \
+    html = "<style>
+html{
+    width:100%;
+    height:100%;
+}
+body{
+    width:100%;
+    height:100%;
+    background-color:#DDD;
+}
+</style><h3>Inan Taksiajat (2.9.-11.10.2019)</h3>" \
            "<b>MA: 8:15 - 13:40<br/>TI: 7:25 - 13:35<br/>KE:    8:15 - 12:20<br/>TO:    7:45 - 13:45<br/>PE:    7:25 - 13:25</b><br/>"
 \
            "<b>Hostname:</b> {hostname}<br/>" \
