cfde5f30ae1ba001f7d58853a9ca9fb190e801bc
Jukka Rajakangas <xapeck@gmail.com>
2019-10-04 08:21:45 +0300

    Update app.py

--- app.py
+++ app.py
@@ -14,4 +14,4 @@ def ajat():
     return html.format(name=os.getenv("NAME", "world"), hostname=socket.gethostname())
 
 if __name__ == "__main__":
-	app.run(host='0.0.0.0', port=1050)
+	app.run(host='192.168.100.103', port=1050)
