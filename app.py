e726d45b161bef77c8e24305fda1dd9c5f54a8d4
Jukka Rajakangas <xapeck@gmail.com>
2019-10-17 11:29:57 +0300

    Update app.py

--- app.py
+++ app.py
@@ -6,9 +6,11 @@ app = Flask(__name__)
 
 @app.route("/") 
 def ajat():
-    html = "<h3>Inan Taksiajat (2.9.-11.10.2019)</h3>" \
-	"<b>MA: 8:15 - 13:40<br/>TI: 7:25 - 13:35<br/>KE: 8:15 - 12:20<br/>TO: 7:45 - 13:45<br/>PE: 7:25 - 13:25</b><br/>" \
-           "<b>Hostname:</b> {hostname}<br/>" 
+    html = "<html><head><meta name="""viewport""" content="""width=device-width, initial-scale=1"""></head><STYLE type="""text/css"""> \
+    BODY { text-align: center}
+    BODY { font-size:140%;}
+    </STYLE><body><h3>Inan Taksiajat (2.9.-11.10.2019)</h3>" \
+    "<b>MA: 8:15 - 13:40<br/>TI: 7:25 - 13:35<br/>KE: 8:15 - 12:20<br/>TO: 7:45 - 13:45<br/>PE: 7:25 - 13:25</b><br/></body>"
     return html.format(name=os.getenv("NAME", "world"), hostname=socket.gethostname())
 
 if __name__ == "__main__":
